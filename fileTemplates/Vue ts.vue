0b29440a81158e909b2d2f101aac856220b4d467
candychuang <candychuang@tencent.com>
2022-03-21 09:53:00 +0800

    WS-2021.3.1 <candychuang@CANDYCHUAN-PC1 Create baseRefactoring.xml, ide.general.xml, ide-features-trainer.xml, project.default.xml, Default copy.xml, user.xml, Default.xml, find.xml, Vue ts.vue, log_highlighting.xml, filetypes.xml, projectView.xml, keymap.xml, colors.scheme.xml, github.xml, templates.xml, laf.xml, sshConfigs.xml, Default.xml, Angular.xml, JavaScript.xml, AngularJS.xml, intentionSettings.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, grazie_global.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/Vue ts.vue
@@ -0,0 +1,14 @@
+<template>
+#[[$END$]]#
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+name: "${COMPONENT_NAME}"
+})
+</script>
+
+<style scoped>
+
+</style>
